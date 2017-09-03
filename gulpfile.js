4f10cfdc8b4677235e48de1c85ad75c9291c6ab4
Mathieu Bergeron <mathieu@infosysteme.org>
2017-09-03 10:03:51 -0400

    Initial commit
    First commit to commit initial structure

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var minify = require('gulp-minify');
+var minifyCss = require('gulp-minify-css'); //minifies css
+var concat = require('gulp-concat');        //joins multiple files into one
+var rename = require('gulp-rename');        //renames files
+var uglify = require('gulp-uglify');
+
+var SCSS_SRC = ['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'];
+var jsFiles = ['src/js/*.js'];
+
+gulp.task('scripts', function() {
+    gulp.src(jsFiles)
+        .pipe(concat('scripts.js'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js')) //writes the renamed file to the destination
+    	.pipe(browserSync.stream())
+});
+
+
+gulp.task('scss', function() {
+  gulp.src(SCSS_SRC)                          //reads all the SASS files
+    .pipe(sass().on('error', sass.logError))  //compiles SASS to CSS and logs errors
+    .pipe(minifyCss())                        //minifies the CSS files 
+    .pipe(concat('style.css'))  //concatenates all the CSS files into one 
+    .pipe(rename({              //renames the concatenated CSS file
+      basename : 'style',       //the base name of the renamed CSS file
+      extname : '.min.css'      //the extension fo the renamed CSS file
+    }))
+    .pipe(gulp.dest('build/css')) //writes the renamed file to the destination
+	.pipe(browserSync.stream())
+});
+
+gulp.task('copy', function () {
+    gulp
+     .src('src/*.html')
+     .pipe(gulp.dest('build'))
+});
+
+gulp.task('serve', ['copy', 'scss', 'scripts'], function() {
+	
+	browserSync.init({
+		server: "./build"
+	});
+	
+	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['scss']);
+	gulp.watch(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'src/js/*.js'], ['scripts']);
+	gulp.watch(['src/*.html'], ['copy']);
+	gulp.watch("build/*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['scss', 'scripts', 'serve']);
